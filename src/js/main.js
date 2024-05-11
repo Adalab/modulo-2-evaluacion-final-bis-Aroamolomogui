fff0ef27cd14fd9abff77b3669e31c14f6cfb3cf
Aroa Pavon <aroapavongarcia@gmail.com>
2024-05-11 20:53:27 +0200

    get and format data

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,41 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+let personData = [];
+
+const getData = () => {
+  //traer datps api
+  fetch("https://randomuser.me/api/?results=10")
+    //pasar datos a json
+    .then((response) => response.json())
+    //datos api formateados
+    .then((dataApi) => {
+      console.log(dataApi.results);
+      //recorro objetos array peronas de api
+      for (const person of dataApi.results) {
+        //traer persona filtrada
+        const personaFiltrada = clearData(person);
+        //añadir persona filtrada al array final
+        personData.push(personaFiltrada);
+      }
+      console.log(personData);
+    });
+};
+//limpiar un elemento de la api
+const clearData = (person) => {
+  const name = person.name.first + person.name.last;
+  const location = person.location.city;
+  const userName = person.login.userName;
+  const img = person.picture.medium;
+
+  const personaFiltrada = {
+    name: name,
+    location: location,
+    userName: userName,
+    img: img,
+    amigo: false,
+  };
+
+  return personaFiltrada;
+};
+
+getData();
