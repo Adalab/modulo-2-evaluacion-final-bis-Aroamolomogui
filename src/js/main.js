ee6fa6d1c475374e98fcc0ab4f4f917c49a4c967
Aroa Pavon <aroapavongarcia@gmail.com>
2024-05-12 19:53:13 +0200

    add variable addfriend

--- src/js/main.js
+++ src/js/main.js
@@ -9,45 +9,71 @@ const getData = () => {
     .then((response) => response.json())
     //datos api formateados
     .then((dataApi) => {
+      console.log("datos api");
       console.log(dataApi.results);
       //recorro objetos array peronas de api
-      for (const person of dataApi.results) {
+      for (let i = 0; i < dataApi.results.length; i++) {
+        const person = dataApi.results[i];
         //traer persona filtrada
-        const personaFiltrada = clearData(person);
+        const personaFiltrada = clearData(person, i);
         //añadir persona filtrada al array final
         personData.push(personaFiltrada);
       }
-
+      console.log("datos persona filtrados");
       console.log(personData);
+      renderAllPerson(personData);
     });
 };
 
 //limpiar un elemento de la api
-const clearData = (person) => {
+const clearData = (person, id) => {
   const name = person.name.first + person.name.last;
   const location = person.location.city;
   const userName = person.login.userName;
   const img = person.picture.medium;
 
   const personaFiltrada = {
+    id: id,
     name: name,
     location: location,
-    userName: userName,
+    c: userName,
     img: img,
-    amigo: false,
+    friend: false,
   };
 
   return personaFiltrada;
 };
-
+// En esta constante he pintado 1º una tarjeta en HTML y lo he traido al JS con la constante let html entre comillas francesas por ques e un string
 const renderOnePerson = (person) => {
-  let html = "";
+  let html = ` <li class="card js_card_person" id="${person.id}">
+  <img src="${person.img}" alt="image" />
+  <h1>${person.name}</h1>
+  <h2>${person.location}</h2>
+  <h2>${person.userName}</h2>
+</li>`;
+
   return html;
 };
+// con readerAllperson recorro mi arrayb para por cada elemento pintar los datos que necesito//
 const renderAllPerson = (allPerson) => {
   ulList.innerHTML = "";
   for (let i = 0; i < allPerson.length; i++) {
+    //aqui con renderOne person le estoy añadiendo los datos de cada uno que lo contiene essa función//
     ulList.innerHTML += renderOnePerson(allPerson[i]);
   }
+  const allCardPersonLi = document.querySelectorAll(".js_card_person");
+  console.log("li que es?");
+  console.log(allCardPersonLi);
+  //añado el evento para que lo pueda clicar y cuando lo haga después haga otra cosa (añadir las personas como amigas)//
+  for (const li of allCardPersonLi) {
+    li.addEventListener("click", addFavoriteFriend);
+  }
 };
+
+const addFavoriteFriend = (ev) => {
+  //con este current lo que hago es traerme el valor de un id para poder saber sobre quién estoy clicando el id lo creo a mano por que no me convence el de la API//
+  console.log("Qué tiene el current");
+  console.log(ev.currentTarget.id);
+};
+
 getData();
